a356a5b2ea81057cf9de525f71d4a47a699a07d1
s41173 <sanjaya.kiran@gmail.com>
2026-03-12 18:17:42 +0700

    fix env loading

--- models/setup.go
+++ models/setup.go
@@ -15,6 +15,13 @@ var DB *gorm.DB
 
 func ConnectDatabase() {
 
+	// debug env (hapus nanti kalau sudah jalan)
+	log.Println("DB_USER:", os.Getenv("DB_USER"))
+	log.Println("DB_HOST:", os.Getenv("DB_HOST"))
+	log.Println("DB_PORT:", os.Getenv("DB_PORT"))
+	log.Println("DB_NAME:", os.Getenv("DB_NAME"))
+
+	// TLS config untuk MySQL cloud
 	tlsConfig := &tls.Config{
 		InsecureSkipVerify: true,
 	}
@@ -24,6 +31,7 @@ func ConnectDatabase() {
 		log.Fatal(err)
 	}
 
+	// buat DSN dari environment variable
 	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?tls=custom&parseTime=true",
 		os.Getenv("DB_USER"),
@@ -33,10 +41,15 @@ func ConnectDatabase() {
 		os.Getenv("DB_NAME"),
 	)
 
+	log.Println("Connecting to DB:", dsn)
+
+	// koneksi database
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Panicf("Failed to connect database: %v", err)
 	}
+
+	log.Println("Database connected successfully")
 
 	DB = database
 }
