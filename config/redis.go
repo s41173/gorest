02428c707e592171af8355f16d832512185fe5e2
s41173 <sanjaya.kiran@gmail.com>
2026-03-13 13:04:58 +0700

    change db redis server

--- config/redis.go
+++ config/redis.go
@@ -19,11 +19,22 @@ func InitRedis() {
 	// })
 
 	// conenction cloud
+	// RDB = redis.NewClient(&redis.Options{
+	// 	// Addr: "localhost:6379",
+	// 	Addr:     "redis-13734.crce185.ap-seast-1-1.ec2.cloud.redislabs.com:13734",
+	// 	Username: "default",
+	// 	Password: "LftGnlhE11Ex73HzjhVr4keSXUnTMFbp",
+	// })
+
+	// railway connection
 	RDB = redis.NewClient(&redis.Options{
 		// Addr: "localhost:6379",
-		Addr:     "redis-13734.crce185.ap-seast-1-1.ec2.cloud.redislabs.com:13734",
+
+		// redis://default:NxNrRWkkqUPdcGNkaUqJfhQLPEzqZaqA@redis.railway.internal:6379
+		// redis://default:NxNrRWkkqUPdcGNkaUqJfhQLPEzqZaqA@mainline.proxy.rlwy.net:49575
+		Addr:     "redis.railway.internal:6379",
 		Username: "default",
-		Password: "LftGnlhE11Ex73HzjhVr4keSXUnTMFbp",
+		Password: "NxNrRWkkqUPdcGNkaUqJfhQLPEzqZaqA",
 	})
 
 	_, err := RDB.Ping(Ctx).Result()
